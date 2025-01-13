dcd4e6dce0c062fe718a202c971189dc689bd8f4
Chris Mountford <chris.mountford@gmail.com>
2025-01-13 11:59:54 +1100

    add video consumer skeleton

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,41 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import {Box, Typography} from "@mui/material";
+import {VideoCamera, VisionConsumer} from "../mocap/VideoCamera.tsx";
+
+function mkPunterDetector(): VisionConsumer {
+  return {
+    video: async (_video: HTMLVideoElement, _startTimeMs: number, _deltaMs: number): Promise<void> => {
+      //console.log(`consumeVideo: start ${startTimeMs}ms ${deltaMs} ms`);
+      return Promise.resolve();
+    },
+    image: async (_image: HTMLImageElement): Promise<void> => {
+      console.log(`consumeImage`);
+      return Promise.resolve();
+    }
+  };
+}
 
 const Home = () => {
+
+  const tempVc: VisionConsumer[] = [mkPunterDetector()];
   const [count, setCount] = useState(0);
   return (
     <div className="App-body">
-      <h1>Home</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <Typography variant="h2">Cutting Shapes</Typography>
+      <Box className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
-      <div>
+      </Box>
+      <Box>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
         laborum.
-      </div>
+      </Box>
+      <Box sx={{border: "orange dotted thick", p: 5}}>
+        <canvas id="main_view"></canvas>
+        <VideoCamera consumers={tempVc}/>
+      </Box>
     </div>
   );
 };
