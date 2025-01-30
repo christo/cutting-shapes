09cacedbd85db7211049dbc0ff2762688df34834
Chris Mountford <chris.mountford@gmail.com>
2025-01-30 17:31:46 +1100

    tidy

--- src/components/SkeletalDiagnostics.tsx
+++ src/components/SkeletalDiagnostics.tsx
@@ -4,7 +4,13 @@ import { Rot } from '../geometry/la.ts';
 import { Pose } from '../mocap/Pose.ts';
 import { PoseSystem } from '../mocap/PoseSystem.ts';
 
-function Pyr({ part, rot }: { part: string, rot: Rot }) {
+/**
+ * Show Pitch, Roll and Yaw values with the given title.
+ * @param title label for these stats.
+ * @param rot the 3-axis rotation values to show.
+ * @constructor
+ */
+function Pyr({ title, rot }: { title: string, rot: Rot }) {
   const panelSx = {
     textAlign: 'end',
     backgroundColor: 'rgba(255, 80, 80, 0.05)',
@@ -20,7 +26,7 @@ function Pyr({ part, rot }: { part: string, rot: Rot }) {
     </>
   );
   return <Grid2 container spacing={0.2} sx={panelSx}>
-    <Grid2 size={12} sx={{ textAlign: 'end'}}>{part}</Grid2>
+    <Grid2 size={12} sx={{ textAlign: 'end'}}>{title}</Grid2>
     {stat(rot.pitch, 'P')}
     {stat(rot.yaw, 'Y')}
     {stat(rot.roll, 'R')}
@@ -50,17 +56,21 @@ function SkeletalDiagnostics({ poseSystem }: { poseSystem: PoseSystem }) {
     <Box>
       <Stack>
         {poses.map((p: Pose, pi: number) => {
+          const sr = p.skeletalRotation;
           return <Stack direction="row" key={`sd_${pi}`}>
-            <Box sx={{mr: 1}}><Typography sx={{fontWeight: 'bold', fontSize: "large"}}>{pi + 1}</Typography>
+            <Box sx={{mr: 1}}>
+              <Typography sx={{fontWeight: 'bold', fontSize: "large"}}>
+                {pi + 1}
+              </Typography>
             </Box>
             <Box sx={{ display: 'flex', flexFlow: 'row wrap', gap: 1 }} >
-              <Pyr part="HEAD" rot={p.skeletalRotation.head} />
-              <Pyr part="NECK" rot={p.skeletalRotation.neck} />
-              <Pyr part="SPINE" rot={p.skeletalRotation.spine} />
-              <Pyr part="ARM UL" rot={p.skeletalRotation.leftArm} />
-              <Pyr part="ARM UR" rot={p.skeletalRotation.rightArm} />
-              <Pyr part="ARM LL" rot={p.skeletalRotation.leftForearm} />
-              <Pyr part="ARM LR" rot={p.skeletalRotation.rightForearm} />
+              <Pyr title="HEAD" rot={sr.head} />
+              <Pyr title="NECK" rot={sr.neck} />
+              <Pyr title="SPINE" rot={sr.spine} />
+              <Pyr title="ARM UL" rot={sr.leftArm} />
+              <Pyr title="ARM UR" rot={sr.rightArm} />
+              <Pyr title="ARM LL" rot={sr.leftForearm} />
+              <Pyr title="ARM LR" rot={sr.rightForearm} />
             </Box>
           </Stack>;
         })}
