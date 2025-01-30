9aa98d476cc04d1d71c2e74f176345710bdfe89b
Chris Mountford <chris.mountford@gmail.com>
2025-01-30 17:12:12 +1100

    placeholder test

---
+++ src/mocap/SkeletalRotation.test.ts
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+
+// TODO real test
+describe("Nothing Test", () => {
+  it("should return 5 when 2 is added to 3", () => {
+    assert.equal("foo".length, 3);
+  });
+});
