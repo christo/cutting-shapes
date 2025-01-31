34cc0563632812a02966397c7f38aba7ee3f9d4e
Chris Mountford <chris.mountford@gmail.com>
2025-01-31 18:44:56 +1100

    subtype with debug info

--- src/analysis/Rot.ts
+++ src/analysis/Rot.ts
@@ -4,5 +4,9 @@ export interface Rot {
   roll: number;
 }
 
+export interface DebugRot extends Rot {
+  debug: string[],
+}
+
 export const ROT_ZERO: Rot = { yaw: 0, pitch: 0, roll: 0 };
 export const ROT_UNIT: Rot = { yaw: 1, pitch: 1, roll: 1 };
