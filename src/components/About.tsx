16cd2044ea6f9086aa5ad7aa59a1df8e600c9b01
Chris Mountford <chris.mountford@gmail.com>
2025-02-01 15:38:57 +1100

    layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,25 +1,24 @@
-import { Link, List, ListItem } from '@mui/material';
+import { Box, Link, List, ListItem, Typography } from '@mui/material';
 import { Puppet } from '../mocap/Puppet.ts';
 
 
 
 const About = ({puppets}: {puppets: Puppet[]}) => {
   return (
-      <div className="App-body">
+      <Box sx={{m: 10}}>
         <h1>About Cutting Shapes</h1>
-        <div>
+        <Box>
           <i>Created by</i> <b>Chris Mountford</b> <i>January 2025</i>
-        </div>
+        </Box>
         <h2>Acknowledgements</h2>
         <List>
         {puppets.map((puppet, i) => {
           return (<ListItem key={`puppet_ack_${i}`}>
-            <Link href={puppet.src}>{puppet.attribution}
-            </Link> {puppet.license.longName}
+            <Link href={puppet.src}>{puppet.attribution}</Link>&nbsp;<Typography>{puppet.license.longName}</Typography>
           </ListItem>);
         })}
         </List>
-      </div>
+      </Box>
   );
 };
 
