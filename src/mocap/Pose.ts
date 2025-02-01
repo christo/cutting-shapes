c6729b2133d5d9d54dafb42f19ccef0e3aae216d
Chris Mountford <chris.mountford@gmail.com>
2025-02-01 11:33:33 +1100

    clarify

--- src/mocap/Pose.ts
+++ src/mocap/Pose.ts
@@ -4,8 +4,8 @@ import { SkeletalRotation } from './SkeletalRotation.ts';
 export type PoseSupplier = () => Pose[];
 
 /**
- * Project domain representation of body position.
- * Default position is canonical T-pose.
+ * Project domain representation of a single body position.
+ * Default position is canonical T-pose, skeletal rotations are hierarchical bone-relative deltas from t-pose.
  */
 export class Pose {
   skeletalRotation: SkeletalRotation;
